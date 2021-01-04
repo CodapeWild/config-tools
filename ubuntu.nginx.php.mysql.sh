d9448aca8d8821b915b6219a034503d3c84dd9dd
CodapeWild <codape.wild@gmail.com>
2021-01-04 17:31:08 +0800

    add install mysql script into .sh

--- ubuntu.nginx.php.mysql.sh
+++ ubuntu.nginx.php.mysql.sh
@@ -4,3 +4,20 @@
 # nginx:
 # php:
 # mysql:
+
+apt update
+apt upgrade -y
+
+apt install nginx -y
+
+ufw allow "Nginx Full"
+ufw allow ssh
+ufw enable <<<'y'
+ufw status
+
+apt install mysql-server -y
+# mysql_secure_installation <<<'y'
+read -sp "set root@localhost password for mysql": pswd
+mysql <<<"alter user 'root'@'localhost' identified with mysql_native_password by '${pswd}';flush privileges;select user, authentication_string, plugin, host from mysql.user where user='root';"
+
+sysctl -w net.core.somaxconn=4096

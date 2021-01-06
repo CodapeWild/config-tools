10b8df2dbe2bfc3c4bad784a7a2afa17392d87b3
CodapeWild <codape.wild@gmail.com>
2021-01-06 13:36:50 +0800

    add ubuntu system config for ecshop

---
+++ system.ecshop.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -e
+
+apt update
+apt install python-software-properties
+add-apt-repository -y ppa:ondrej/php
+apt update
+apt upgrade -y
+
+apt install nginx -y
+apt install mysql-server -y
+apt install php5.6-fmp php5.6-common php5.6-mysql php5.6-gd -y
+
+read -sp "set root@localhost password for mysql":pswd
+mysql <<<"alter user 'root'@'localhost' identified with mysql_native_password by '${pswd}';flush privileges;select user,host,authentication_string,plugin from mysql.user where user='root';"
+
+ufw allow 'Nginx Full'
+ufw allow ssh
+ufw enable <<<'y'
+ufw status
+
+sysctl -w net.core.somaxconn=4096

2dd2d6547d530d97bba2532188f317c4e40dea4e
CodapeWild <codape.wild@gmail.com>
2021-01-07 13:14:27 +0800

    fix

--- certbot.sh
+++ certbot.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 apt update
-apt upgrade -y
-
+apt install software-properties-common -y
 add-apt-repository ppa:certbot/certbot
+apt update
+apt upgrade -y
 
 apt install python-certbot-nginx -y
 

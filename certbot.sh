16745722fd51861defcfaf8f2ce3fb5c946e6ce5
CodapeWild <codape.wild@gmail.com>
2021-01-08 22:55:01 +0800

    fix certbot installation

--- certbot.sh
+++ certbot.sh
@@ -1,11 +1,6 @@
 #! /bin/bash
 
 apt update
-apt install software-properties-common -y
-add-apt-repository ppa:certbot/certbot
-apt update
-apt upgrade -y
-
-apt install python-certbot-nginx -y
+apt install certbot python-certbot-nginx -y
 
 certbot --nginx
